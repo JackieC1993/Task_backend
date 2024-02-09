614bffe438c06ce2af283cade4704f26a3dd17d0
JackieC1993 <jackiecheung@pursuit.org>
2024-02-09 16:02:17 -0500

    added changes to the gigitnore

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -19,29 +19,41 @@ users.get('/', async (req, res) => {
     }
 })
 
+users.post('/', async (req, res) => {
+  try {
+      const newUser = await createUser(req.body)
+      const token = jwt.sign({ userId: newUser.user_id, username: newUser.username }, secret)
+      res.status(201).json({ user: newUser, token })
+  } catch (err) {
+      res.status(500).json({ error: "Invalid Information", info: err })
+  }
+})
+
+
+
 users.post("/login", async (req, res) => {
-    try {
-      const user = await logInUser(req.body);
-      if (!user) {
-        res.status(401).json({ error: "Invalid username or password" });
-        return; // Exit the function
-      }
-      const token = jwt.sign(
-        { userId: user.user_id, username: user.username },
-        secret
-      );
-  
-      res.status(200).json({
-        user: {
-          user_id: user.user_id,
-          user: user.username,
-          email: user.email,
-        },
-        token,
-      });
-    } catch (err) {
-      res.status(500).json({ error: "Internal user login server error" });
+  try {
+    const user = await logInUser(req.body);
+    if (!user) {
+      res.status(401).json({ error: "Invalid username or password" });
+      return; // Exit the function
     }
-  });
+    const token = jwt.sign(
+      { userId: user.user_id, username: user.username },
+      secret
+    );
+
+    res.status(200).json({
+      user: {
+        user_id: user.user_id,
+        username: user.username,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Internal user login server error" });
+  }
+});
 
 module.exports = users
